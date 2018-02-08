6a73af6a303498961cfe0824d1a0f2bcb543d722
Emily Watkins and Maggie Summers <emily@>
2018-02-08 14:33:08 -0800

    modify method to add 1 to prime to pass test.

--- lib/prime.rb
+++ lib/prime.rb
@@ -5,25 +5,14 @@ class Prime
 
   def sift
     range = (2..@number)
-    # array = [2]
     prime = 2
 
-    new_array = range.reject { |r| (r != prime) & (r%prime == 0) }
+    while prime < 4
+      range = range.reject { |r| (r != prime) & (r%prime == 0) }
 
-    new_array
-
-
-    # while prime < 4
-    #   range.each do |r|
-    #     if (r%prime != 0)
-    #       array.push(r)
-    #     end
-    #   end
-    #   prime = prime + 1
-    #   array
-    # end
-
-
+      prime = prime + 1
+    end
+    range
 
   end
 end
